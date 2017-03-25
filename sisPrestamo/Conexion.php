6614f872873a6b6c7ad9f539b1bfc4bdfabba55e
hopechan <da13002@ues.edu.sv>
2017-03-25 10:57:37 -0600

    Conexion BD

--- sisPrestamo/Conexion.php
+++ sisPrestamo/Conexion.php
@@ -1,40 +1,39 @@
 <?php
 
-
-
-/**
- * Description of Conexion
- *
- * @author esperanza
- */
 class Conexion {
-    var $servidor = "localhost"; //el servidor que utilizaremos, en este caso será el localhost
-    var $usuario = "root"; //El usuario que acabamos de crear en la base de datos
-    var $pass = "1234"; //La contraseña del usuario que utilizaremos
-    var $BD = "prestamos"; //El nombre de la base de datos
- 
-/*Aquí abrimos la conexión en el servidor.
-Normalmente se envian 3 parametros (los datos del servidor, usuario y contraseña) a la función mysql_connect,
-si no hay ningún error la conexión será un éxito
-El @ que se ponde delante de la funcion, es para que no muestre el error al momento de ejecutarse, ya crearemos un código para eso*/
-    var $conexion = mysql_connect(servidor, usuario, pass);
- 
-/* Aquí preguntamos si la conexión no pudo realizarse, de ser así lanza un mensaje en la pantalla con el siguiente texto "No pudo conectarse:"
-y le agrega el error ocurrido con "mysql_error()"
-*/
-    if (!$conexion) {
-        die('<strong>No pudo conectarse:</strong> ' . mysql_error());
-    }else{
-//La siguiente linea no es necesaria al momento de programar, simplemente la pondremos ahora para poder observar que la conexión ha sido realizada
-    echo 'Conectado  satisfactoriamente al servidor <br />';
-}
-/*En la siguiente linea seleccionaremos la BD con la que trabajaremos y le pasaremos como referencia la conexión al servidor
-Para saber si se conecto o no a la BD podríamos utilizar el IF de la misma forma que la utilizamos al momento de conectar al servidor,
-pero les muestro otra forma de comprobar eso, es un poco mas corto el código.
-*/
-mysql_select_db($BD, $conexion) or die(mysql_error($conexion));
- 
-//Luego de abrir y utilizar una conexión en MySql siempre hay que cerrarla luego de terminar de trabajar con ella
-mysql_close($conexion);
-?>
+  private $conexion; 
+  private $total_consultas;
+  
+  public function MySQL(){ 
+    if(!isset($this->conexion)){
+      $this->conexion = (mysql_connect("localhost","root","1234"))
+        or die(mysql_error());
+      mysql_select_db("prestamos",$this->conexion) or die(mysql_error());
+    }
+  }
+  
+  public function consulta($consulta){ 
+    $this->total_consultas++; 
+    $resultado = mysql_query($consulta,$this->conexion);
+    if(!$resultado){ 
+      echo 'MySQL Error: ' . mysql_error();
+      exit;
+    }
+    return $resultado;
+  }
+  
+  public function fetch_array($consulta){
+   return mysql_fetch_array($consulta);
+  }
+  
+  public function num_rows($consulta){
+   return mysql_num_rows($consulta);
+  }
+  
+  public function getTotalConsultas(){
+   return $this->total_consultas; 
+  }
+
+  
+  }?>
 }
