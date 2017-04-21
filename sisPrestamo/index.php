ef6d8a72a0c747376e60f38acec801fc75932477
hopechan <da13002@ues.edu.sv>
2017-04-21 14:17:36 -0600

    sisPrestamo

--- sisPrestamo/index.php
+++ sisPrestamo/index.php
@@ -2,28 +2,56 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Sistema Prestamo</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/style.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     <body>
-        <?php
-            require_once("Conexion.php");
-            $con = new Conexion();
-            $var = 0;
-            $sql =$con->prepare("INSERT INTO cardinales(numero) VALUES (:numero)");
-            
-            #while ($var <= 10) {
-            #    $sql->bindParam(":numero", $var);
-            #    $sql->execute();
-            #    $var++;
-            #}
-            
-            $consulta = $con->prepare("DELETE FROM cardinales");
-            $consulta->execute();
-            $cliente = $consulta->fetchAll();
-            var_dump($cliente);
-            
-            
-            
-        ?>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Sistema Prestamo</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="w3-sidebar w3-bar-block" style="width:15%">
+            <a href="#" class="w3-bar-item w3-button">Clientes</a>
+            <a href="#" class="w3-bar-item w3-button">Pago</a>
+            <a href="#" class="w3-bar-item w3-button">Prestamo</a>
+        </div>
+        <div>
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <td>DUI</td>
+                        <td>NIT</td>
+                        <td>NOMBRE</td>
+                        <td>APELLIDOS</td>
+                        <td>SEXO</td>
+                        <td>FECHA <br>DE NACIMIENTO</td>
+                        <td>DIRECCION</td>
+                        <td>TELEFONO</td>
+                        <td>BOTONES</td>
+                    </tr>
+                </thead>
+                <tr>
+                    <td>1111111111</td>
+                    <td>1111111111</td>
+                    <td>1111111111</td>
+                    <td>1111111111</td>
+                    <td>1111111111</td>
+                    <td>1111111111</td>
+                    <td>1111111111</td>
+                    <td>1111111111</td>
+                    <td>1111111111</td>
+                </tr>
+            </table>
+        </div>
     </body>
 </html>
