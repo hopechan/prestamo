f99a9848b16cdc05a4420c066dcd3339a6c8f178
hopechan <da13002@ues.edu.sv>
2017-04-22 11:02:15 -0600

    sisPrestamo

--- sisPrestamo/index.php
+++ sisPrestamo/index.php
@@ -9,18 +9,15 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
-    <body>
+    <body id="body">
         <nav class="navbar navbar-default">
             <div class="container-fluid">
                 <div class="navbar-header">
                     <a class="navbar-brand" href="#">Sistema Prestamo</a>
                 </div>
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Clientes</a></li>
-                </ul>
             </div>
         </nav>
-        <div class="w3-sidebar w3-bar-block" style="width:15%">
+        <div class="w3-sidebar w3-bar-block" id="sidebar">
             <a href="#" class="w3-bar-item w3-button">Clientes</a>
             <a href="#" class="w3-bar-item w3-button">Pago</a>
             <a href="#" class="w3-bar-item w3-button">Prestamo</a>
